5ac0b1215bd1ac42e6f22e4ba1ac6e02ae885d3a
Napat Srichan <me@napatsc.com>
2026-01-20 15:48:47 +0700

    add concurrency/parallelism to llm request
    improvement can be made

--- src/translate.rs
+++ src/translate.rs
@@ -9,71 +9,86 @@ use async_openai::{
     },
 };
 
-pub async fn task(src: Vec<String>) -> Result<Vec<String>> {
-    let client = Client::default();
-    let system_prompt = std::fs::read_to_string(std::path::PathBuf::from("./tren-sys-prompt.txt"))?;
+async fn chat(
+    client: &Client<OpenAIConfig>,
+    model: &String,
+    sys_prompt: &String,
+    user_prompt: &String,
+) -> Result<String> {
+    // simple case
+    if user_prompt.trim().len() == 0 {
+        return Ok(user_prompt.clone());
+    }
 
-    let mut results = Vec::with_capacity(src.len());
-    for mipc in src {
-        // simple case
-        if mipc.trim().len() == 0 {
-            results.push(mipc.clone());
+    let mut attempts = 1u8;
+    loop {
+        let request = CreateChatCompletionRequestArgs::default()
+            .model(model)
+            .messages(vec![
+                ChatCompletionRequestSystemMessage::from(sys_prompt.clone()).into(),
+                ChatCompletionRequestUserMessage::from(user_prompt.clone()).into(),
+            ])
+            .n(1)
+            .build()?;
+
+        let response = client
+            .chat()
+            .create(request)
+            .await?
+            .choices
+            .first()
+            .ok_or(anyhow!("no choices?"))?
+            .message
+            .content
+            .clone()
+            .ok_or(anyhow!("content error"))?;
+
+        // special token check
+        let src_tok_count = user_prompt.chars().filter(|c| *c == TOK_SEP).count();
+        let tar_tok_count = response.chars().filter(|c| *c == TOK_SEP).count();
+        if src_tok_count != tar_tok_count {
+            attempts += 1;
             continue;
         }
 
-        let mut attempts = 1u8;
-        loop {
-            let request = CreateChatCompletionRequestArgs::default()
-                .model("gpt-5-nano")
-                .messages(vec![
-                    ChatCompletionRequestSystemMessage::from(system_prompt.clone()).into(),
-                    ChatCompletionRequestUserMessage::from(mipc.clone()).into(),
-                ])
-                .n(1)
-                .build()?;
-
-            let response = client
-                .chat()
-                .create(request)
-                .await?
-                .choices
-                .first()
-                .ok_or(anyhow!("no choices?"))?
-                .message
-                .content
-                .clone()
-                .ok_or(anyhow!("content error"))?;
-
-            // special token check
-            let src_tok_count = mipc.chars().filter(|c| *c == TOK_SEP).count();
-            let tar_tok_count = response.chars().filter(|c| *c == TOK_SEP).count();
-            if src_tok_count != tar_tok_count {
-                attempts += 1;
-                continue;
-            }
-
-            println!(
-                "--- Source {}---
+        println!(
+            "--- Source ---
 {}
---- Target ---
+--- Target {}---
 {}
 ",
-                if attempts > 1 {
-                    format!("(attempt {attempts})")
-                } else {
-                    "".to_string()
-                },
-                mipc,
-                response
-            );
+            user_prompt,
+            if attempts > 1 {
+                format!("(attempt {attempts})")
+            } else {
+                "".to_string()
+            },
+            response
+        );
 
-            // some check login
-
-            results.push(response);
-
-            break;
-        }
+        return Ok(response);
     }
+}
+
+use futures::{StreamExt, stream};
+
+pub async fn task(src: Vec<String>) -> Result<Vec<String>> {
+    let client = Client::default();
+    let model = "gpt-5-nano".to_string();
+    let system_prompt = std::fs::read_to_string(std::path::PathBuf::from("./tren-sys-prompt.txt"))?;
+
+    let processings = stream::iter(src)
+        .map(|mipc| {
+            let client = client.clone();
+            let model = model.clone();
+            let system_prompt = system_prompt.clone();
+            async move { chat(&client, &model, &system_prompt, &mipc).await }
+        })
+        .buffer_unordered(4)
+        .collect::<Vec<_>>()
+        .await;
+
+    let results = processings.into_iter().collect::<Result<Vec<_>>>()?;
 
     Ok(results)
 }
