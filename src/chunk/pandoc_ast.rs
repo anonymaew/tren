9ceaafa6d2bb9bad1ccc7574a51705985ba108b5
Napat Srichan <me@napatsc.com>
2026-01-20 16:27:33 +0700

    refactor tree traverse

--- src/chunk/pandoc_ast.rs
+++ src/chunk/pandoc_ast.rs
@@ -5,107 +5,102 @@ use serde_json::json;
 use std::path::PathBuf;
 use std::vec::IntoIter;
 
-fn inlines_to_mipc(inlines: Vec<Inline>) -> String {
-    fn inlines_to_strings(ins: &Vec<Inline>) -> Vec<String> {
-        ins.iter()
-            .flat_map(|inline| match inline {
-                Inline::Str(str) | Inline::RawInline(_, str) => vec![str.clone()],
-                Inline::Code(_, _)
-                | Inline::Space
-                | Inline::SoftBreak
-                | Inline::LineBreak
-                | Inline::Math(_, _)
-                | Inline::Note(_) => vec![],
-                Inline::Cite(_, _ins) => todo!(),
-                Inline::Span(_, ins)
-                | Inline::Link(_, ins, _)
-                | Inline::Image(_, ins, _)
-                | Inline::Emph(ins)
-                | Inline::Strong(ins)
-                | Inline::Underline(ins)
-                | Inline::Strikeout(ins)
-                | Inline::Superscript(ins)
-                | Inline::Subscript(ins)
-                | Inline::SmallCaps(ins)
-                | Inline::Quoted(_, ins) => inlines_to_strings(ins),
-            })
-            .collect()
-    }
+fn collect_ins(bs: &Vec<Block>) -> Vec<String> {
+    bs.iter()
+        .filter_map(|b| match b {
+            Block::Plain(ins) | Block::Para(ins) | Block::Header(_, _, ins) => {
+                fn inlines_to_strings(ins: &Vec<Inline>) -> Vec<String> {
+                    ins.iter()
+                        .flat_map(|inline| match inline {
+                            Inline::Str(str) | Inline::RawInline(_, str) => vec![str.clone()],
+                            Inline::Code(_, _)
+                            | Inline::Space
+                            | Inline::SoftBreak
+                            | Inline::LineBreak
+                            | Inline::Math(_, _)
+                            | Inline::Note(_) => vec![],
+                            Inline::Cite(_, _ins) => todo!(),
+                            Inline::Span(_, ins)
+                            | Inline::Link(_, ins, _)
+                            | Inline::Image(_, ins, _)
+                            | Inline::Emph(ins)
+                            | Inline::Strong(ins)
+                            | Inline::Underline(ins)
+                            | Inline::Strikeout(ins)
+                            | Inline::Superscript(ins)
+                            | Inline::Subscript(ins)
+                            | Inline::SmallCaps(ins)
+                            | Inline::Quoted(_, ins) => inlines_to_strings(ins),
+                        })
+                        .collect()
+                }
 
-    inlines_to_strings(&inlines).join(&TOK_SEP.to_string())
-}
-
-fn mipc_to_inlines(ins_src: &mut Vec<Inline>, mipc: String) {
-    let strings = mipc
-        .split(TOK_SEP)
-        .map(|s| s.to_string())
-        .collect::<Vec<_>>();
-
-    fn strings_to_inlines(inlines: &mut Vec<Inline>, str_iter: &mut IntoIter<String>) {
-        inlines.iter_mut().for_each(|inline| match inline {
-            Inline::Str(i) | Inline::RawInline(_, i) => {
-                *i = str_iter.next().expect("mismatch");
+                Some(vec![inlines_to_strings(ins).join(&TOK_SEP.to_string())])
             }
-            Inline::Code(_, _)
-            | Inline::Space
-            | Inline::SoftBreak
-            | Inline::LineBreak
-            | Inline::Math(_, _)
-            | Inline::Note(_) => (),
-            Inline::Cite(_, _ins) => todo!(),
-            Inline::Span(_, ins)
-            | Inline::Link(_, ins, _)
-            | Inline::Image(_, ins, _)
-            | Inline::Emph(ins)
-            | Inline::Strong(ins)
-            | Inline::Underline(ins)
-            | Inline::Strikeout(ins)
-            | Inline::Superscript(ins)
-            | Inline::Subscript(ins)
-            | Inline::SmallCaps(ins)
-            | Inline::Quoted(_, ins) => strings_to_inlines(ins, str_iter),
-        });
-    }
-
-    strings_to_inlines(ins_src, &mut strings.into_iter());
+            Block::LineBlock(_inss) => todo!(),
+            Block::CodeBlock(_, _) => None,
+            Block::RawBlock(_format, _text) => None,
+            Block::BlockQuote(bs) | Block::Div(_, bs) => Some(collect_ins(bs)),
+            Block::OrderedList(_, _bss) => todo!(),
+            Block::BulletList(_bss) => todo!(),
+            Block::DefinitionList(_terms) => todo!(),
+            Block::HorizontalRule | Block::Null => None,
+            Block::Table(_) => todo!(),
+            Block::Figure(_, _cap, _bs) => todo!(),
+        })
+        .flatten()
+        .collect::<Vec<_>>()
 }
 
-fn collect_ins(b: &Block) -> Vec<Vec<Inline>> {
-    match b {
-        Block::Plain(ins) | Block::Para(ins) | Block::Header(_, _, ins) => vec![ins.clone()],
-        Block::LineBlock(_inss) => todo!(),
-        Block::CodeBlock(_, _) => vec![],
-        Block::RawBlock(_format, _text) => vec![],
-        Block::BlockQuote(bs) | Block::Div(_, bs) => {
-            bs.iter().flat_map(|b| collect_ins(&b)).collect()
-        }
-        Block::OrderedList(_, _bss) => todo!(),
-        Block::BulletList(_bss) => todo!(),
-        Block::DefinitionList(_terms) => todo!(),
-        Block::HorizontalRule | Block::Null => vec![],
-        Block::Table(_) => todo!(),
-        Block::Figure(_, _cap, _bs) => todo!(),
-    }
-}
-
-fn apply_mipc_to_block(b_ref: &mut Block, mipc_iter: &mut IntoIter<String>) {
-    match b_ref {
+fn apply_mipc_to_blocks(bs_ref: &mut Vec<Block>, mipc_iter: &mut IntoIter<String>) {
+    bs_ref.iter_mut().for_each(|b_ref| match b_ref {
         Block::Plain(ins) | Block::Para(ins) | Block::Header(_, _, ins) => {
-            mipc_to_inlines(ins, mipc_iter.next().expect("mismatch"));
+            let strings = mipc_iter
+                .next()
+                .expect("mismatch")
+                .split(TOK_SEP)
+                .map(|s| s.to_string())
+                .collect::<Vec<_>>();
+
+            fn strings_to_inlines(inlines: &mut Vec<Inline>, str_iter: &mut IntoIter<String>) {
+                inlines.iter_mut().for_each(|inline| match inline {
+                    Inline::Str(i) | Inline::RawInline(_, i) => {
+                        *i = str_iter.next().expect("mismatch");
+                    }
+                    Inline::Code(_, _)
+                    | Inline::Space
+                    | Inline::SoftBreak
+                    | Inline::LineBreak
+                    | Inline::Math(_, _)
+                    | Inline::Note(_) => (),
+                    Inline::Cite(_, _ins) => todo!(),
+                    Inline::Span(_, ins)
+                    | Inline::Link(_, ins, _)
+                    | Inline::Image(_, ins, _)
+                    | Inline::Emph(ins)
+                    | Inline::Strong(ins)
+                    | Inline::Underline(ins)
+                    | Inline::Strikeout(ins)
+                    | Inline::Superscript(ins)
+                    | Inline::Subscript(ins)
+                    | Inline::SmallCaps(ins)
+                    | Inline::Quoted(_, ins) => strings_to_inlines(ins, str_iter),
+                });
+            }
+
+            strings_to_inlines(ins, &mut strings.into_iter());
         }
         Block::LineBlock(_inss) => todo!(),
         Block::CodeBlock(_, _) => (),
         Block::RawBlock(_format, _text) => (),
-        Block::BlockQuote(bs) | Block::Div(_, bs) => bs
-            .iter_mut()
-            .for_each(|b| apply_mipc_to_block(b, mipc_iter)),
+        Block::BlockQuote(bs) | Block::Div(_, bs) => apply_mipc_to_blocks(bs, mipc_iter),
         Block::OrderedList(_, _bss) => todo!(),
         Block::BulletList(_bss) => todo!(),
         Block::DefinitionList(_terms) => todo!(),
         Block::HorizontalRule | Block::Null => (),
         Block::Table(_) => todo!(),
         Block::Figure(_, _cap, _bs) => todo!(),
-    };
+    })
 }
 
 fn clean_space(bs: &mut Vec<Block>) {
@@ -167,7 +162,6 @@ impl AST for PandocAST {
     fn import(&mut self, filepath: PathBuf) -> Result<()> {
         let mut pandoc = pandoc::new();
         pandoc.set_input(pandoc::InputKind::Files(vec![filepath]));
-        // pandoc.set_input_format(pandoc::InputFormat::Markdown, vec![]);
         pandoc.set_output_format(pandoc::OutputFormat::Json, vec![]);
         pandoc.set_output(pandoc::OutputKind::Pipe);
 
@@ -203,19 +197,13 @@ impl AST for PandocAST {
     }
 
     fn to_mipcs(&self) -> Vec<String> {
-        self.ast
-            .iter()
-            .flat_map(collect_ins)
-            .map(inlines_to_mipc)
-            .collect::<Vec<_>>()
+        collect_ins(&self.ast)
     }
 
     fn apply_mipcs(&mut self, mipcs: Vec<String>) -> Result<()> {
         let mut mipc_iter = mipcs.into_iter();
 
-        self.ast.iter_mut().for_each(|block| {
-            apply_mipc_to_block(block, &mut mipc_iter);
-        });
+        apply_mipc_to_blocks(&mut self.ast, &mut mipc_iter);
         Ok(())
     }
 }
