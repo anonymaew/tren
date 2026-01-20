5605f090a37fde5fb91042bd3b25c39b8b5e2a88
Napat Srichan <me@napatsc.com>
2026-01-20 15:44:55 +0700

    add clean space logic
    pandoc ast process to individual space token (not the same as string);
    we dont want that

--- src/chunk/pandoc_ast.rs
+++ src/chunk/pandoc_ast.rs
@@ -1,6 +1,7 @@
 use crate::chunk::{AST, TOK_SEP};
 use anyhow::{Result, anyhow};
 use pandoc_types::definition::{Inline, *};
+use serde_json::json;
 use std::path::PathBuf;
 use std::vec::IntoIter;
 
@@ -107,6 +108,56 @@ fn apply_mipc_to_block(b_ref: &mut Block, mipc_iter: &mut IntoIter<String>) {
     };
 }
 
+fn clean_space(bs: &mut Vec<Block>) {
+    bs.iter_mut().for_each(|b| match b {
+        Block::Plain(ins) | Block::Para(ins) | Block::Header(_, _, ins) => {
+            fn clean_space_inlines(ins: &mut Vec<Inline>) {
+                ins.iter_mut().for_each(|inline| {
+                    match inline {
+                        Inline::Space => *inline = Inline::Str(" ".to_string()),
+                        Inline::Link(_, ins, _)
+                        | Inline::Image(_, ins, _)
+                        | Inline::Emph(ins)
+                        | Inline::Strong(ins)
+                        | Inline::Underline(ins)
+                        | Inline::Strikeout(ins)
+                        | Inline::Superscript(ins)
+                        | Inline::Subscript(ins)
+                        | Inline::SmallCaps(ins)
+                        | Inline::Quoted(_, ins) => clean_space_inlines(ins),
+                        _ => (),
+                    };
+                });
+                *ins = ins
+                    .iter()
+                    .fold(vec![], |accs: Vec<Inline>, inline: &Inline| {
+                        match (accs.last(), inline) {
+                            (None, _) => vec![inline.clone()],
+                            (Some(Inline::Str(str1)), Inline::Str(str2)) => {
+                                let mut new_accs = accs.clone();
+                                *new_accs.last_mut().unwrap() =
+                                    Inline::Str(format!("{str1}{str2}"));
+                                new_accs
+                            }
+                            _ => accs.to_vec(),
+                        }
+                    })
+            }
+            clean_space_inlines(ins);
+        }
+        Block::LineBlock(_inss) => todo!(),
+        Block::CodeBlock(_, _) => (),
+        Block::RawBlock(_format, _text) => (),
+        Block::BlockQuote(bs) | Block::Div(_, bs) => clean_space(bs),
+        Block::OrderedList(_, _bss) => todo!(),
+        Block::BulletList(_bss) => todo!(),
+        Block::DefinitionList(_terms) => todo!(),
+        Block::HorizontalRule | Block::Null => (),
+        Block::Table(_) => todo!(),
+        Block::Figure(_, _cap, _bs) => todo!(),
+    });
+}
+
 #[derive(Clone, Default)]
 pub struct PandocAST {
     ast: Vec<Block>,
@@ -116,7 +167,7 @@ impl AST for PandocAST {
     fn import(&mut self, filepath: PathBuf) -> Result<()> {
         let mut pandoc = pandoc::new();
         pandoc.set_input(pandoc::InputKind::Files(vec![filepath]));
-        pandoc.set_input_format(pandoc::InputFormat::Markdown, vec![]);
+        // pandoc.set_input_format(pandoc::InputFormat::Markdown, vec![]);
         pandoc.set_output_format(pandoc::OutputFormat::Json, vec![]);
         pandoc.set_output(pandoc::OutputKind::Pipe);
 
@@ -127,21 +178,27 @@ impl AST for PandocAST {
         }?;
         let ast = serde_json::from_str::<Pandoc>(&result_buf)?;
         self.ast = ast.blocks;
+
+        clean_space(&mut self.ast);
+
         Ok(())
     }
 
     fn export(&self, filepath: PathBuf) -> Result<()> {
         let mut pandoc = pandoc::new();
-        pandoc.set_input(pandoc::InputKind::Pipe(serde_json::to_string(&self.ast)?));
+        pandoc.set_input(pandoc::InputKind::Pipe(
+            json!({
+                "pandoc-api-version": [1,23,1],
+                "meta": {},
+                "blocks": &self.ast
+            })
+            .to_string(),
+        ));
         pandoc.set_input_format(pandoc::InputFormat::Json, vec![]);
         pandoc.set_output_format(pandoc::OutputFormat::Markdown, vec![]);
         pandoc.set_output(pandoc::OutputKind::File(filepath));
 
-        let result = pandoc.execute()?;
-        match result {
-            pandoc::PandocOutput::ToBuffer(buf) => Ok(buf),
-            _ => Err(anyhow!("buf not found?")),
-        }?;
+        pandoc.execute()?;
         Ok(())
     }
 
